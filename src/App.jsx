fbff32b0f3a024d82014f6d8a3f6a879b03a5b14
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-27 10:53:53 +1100

    set up data and overall styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import uuid from "uuid";
+
+const itemsData = [
+  { id: uuid(), content: "item 1" },
+  { id: uuid(), content: "item 2" },
+];
+
+const columnsData = [
+  {
+    [uuid()]: {
+      name: "Group 1",
+      items: itemsData,
+    },
+  },
+];
+
 function App() {
-  return <div className=""></div>;
+  const [columns, setColumns] = useState(columnsData);
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      <DragDropContext
+        onDragEnd={(result) => console.log(result)}
+      ></DragDropContext>
+    </div>
+  );
 }
 
 export default App;
