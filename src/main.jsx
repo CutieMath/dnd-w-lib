6fe65fac0e6810654a6787c666044784be15331c
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-27 15:30:36 +1100

    add comments about the strict mode support

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,4 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// Follow up on the strict mode support for React18: https://github.com/atlassian/react-beautiful-dnd/issues/2350
